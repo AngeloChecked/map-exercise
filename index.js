246c780b21527855883c10338d68d83eb8afdc63
angelochecked <angi99552@gmail.com>
2023-10-15 20:33:53 +0200

    setup sidebar

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
+const campaings = [
+	{
+		name: "campaignName", description: "campaignDescription",
+		missions: [
+			{ name: "missionName", completed: false, area: {} }
+		]
+	}
+]
+
 window.onload = () => {
 	initMap(map)
+	const campaingsContent = document.getElementById('campaigns-content')
+	const mapperContnent = document.getElementById('mapper-content')
 }
 
-function initMap(_map){
+function initMap(_map) {
 	var map = L.map('map').setView([51.505, -0.09], 13);
 
 	L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+		maxZoom: 19,
+	}).addTo(map);
+
+	L.control.sidebar({
+		autopan: false,
+		closeButton: true,
+		container: 'sidebar',
+		position: 'left',
 	}).addTo(map);
 
 	return map
+}
+
+function paneTitle(content) {
+	const title = document.createElement("h1")
+	title.innerText = content
+	return title
 }
